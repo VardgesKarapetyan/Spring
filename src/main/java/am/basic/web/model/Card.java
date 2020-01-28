139fea276d0b822443858f93327ee0e6408e271e
DELL <anna.gspoyan.2000@gmail.com>
2020-01-28 05:50:25 -0800

    dhuchd

--- src/main/java/am/basic/web/model/Card.java
+++ src/main/java/am/basic/web/model/Card.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 @Entity
 @Data
 public class Card {
+    /*asfvbisdhmfvjovhdabvhisdajkbvihkvw*/
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  int id;
